37b03fe485ac518432ef0b1d8f0d358047cd562b
LaetitiaVictor <68911584+LaetitiaVictor@users.noreply.github.com>
2021-02-16 22:30:09 +0100

    Servlet accueil : recherche article par categorie

--- Encheres/src/fr/eni/Encheres/servlet/ServletAccueil.java
+++ Encheres/src/fr/eni/Encheres/servlet/ServletAccueil.java
@@ -1,11 +1,16 @@
 package fr.eni.Encheres.servlet;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import fr.eni.Encheres.bll.ArticleVenduManager;
+import fr.eni.Encheres.bo.ArticleVendu;
 
 /**
  * Servlet implementation class ServletAccueil
@@ -14,27 +19,24 @@ import javax.servlet.http.HttpServletResponse;
 public class ServletAccueil extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ServletAccueil() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		ArticleVenduManager articleVenduManager = new ArticleVenduManager();
+		List<ArticleVendu> listeArticleVendu = null;
+		request.setAttribute("categorie", request.getParameter("categorie"));
+		switch("categorie") {
+			case "Informatique" : ArticleVenduManager.selectByCategorie();
+			break;
+		}
+	
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
