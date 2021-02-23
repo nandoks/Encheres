cb7aef5fed249c50e698a3718f8caae2c1a426b0
Fernando Oksman <fernandooksman@gmail.com>
2021-02-23 07:40:44 -0300

    commit retrait DAO

---
+++ Encheres/WebContent/WEB-INF/fragments/enteteDeconnecte.jsp
@@ -0,0 +1,12 @@
+<meta charset="utf-8">
+<div class="div-block-2">
+	<img src="images/ENIEcole-1.jpg" loading="lazy"
+		sizes="(max-width: 479px) 100vw, 75px" alt="" class="image-5">
+</div>
+<a href="index.html" aria-current="page"
+	class="lien-block ligne-block rangee1">
+	<h1 class="titre">ENI Enchères</h1>
+</a>
+<a href="${pageContext.request.contextPath}/authentification"
+	class="lien">Connection</a>
+<a href="${pageContext.request.contextPath}/creationCompte" class="lien">Inscription</a>
---
+++ Encheres/src/fr/eni/Encheres/dal/RetraitDAOJdbcImpl.java
@@ -0,0 +1,70 @@
+package fr.eni.Encheres.dal;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import fr.eni.Encheres.bo.Retrait;
+
+public class RetraitDAOJdbcImpl implements RetraitDAO{
+
+	public final String sqlInsert = "INSERT INTO retraits (no_article, rue, code_postal, ville)"
+			+ "values (?,?,?,?)";
+	
+	public final String sqlSelectById = "SELECT rue, code_postal, ville FROM retraits where no_article = ?";
+	
+	@Override
+	public List<Retrait> selectAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Retrait selectById(int id) {
+		Retrait retrait = new Retrait();
+		try(Connection conn = ConnectionProvider.getConnection();
+				PreparedStatement prst = conn.prepareStatement(sqlInsert, PreparedStatement.RETURN_GENERATED_KEYS)){
+			prst.setInt(1, id);
+			ResultSet rs = prst.executeQuery();
+			if(rs.next()) {
+				retrait.setRue(rs.getString("rue"));
+				retrait.setCode_postal(rs.getString("code_postal"));
+				retrait.setVille(rs.getString("ville"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return retrait;
+	}
+
+	@Override
+	public void insert(Retrait retrait) {
+		try(Connection conn = ConnectionProvider.getConnection();
+				PreparedStatement prst = conn.prepareStatement(sqlSelectById)){
+			int index = 1;
+			prst.setInt(index++, retrait.getNumeroArticle());
+			prst.setString(index++, retrait.getRue());
+			prst.setString(index++, retrait.getCode_postal());
+			prst.setString(index++, retrait.getVille());
+			prst.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	public void update(Retrait obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
