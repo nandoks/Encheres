733e22dc5ff411e861f80a4d4190e2e51d2c05ce
Fernando Oksman <fernandooksman@gmail.com>
2021-02-25 07:10:55 -0300

    RetraitManager et DAOFactory mis a jour

--- Encheres/src/fr/eni/Encheres/bll/RetraitManager.java
+++ Encheres/src/fr/eni/Encheres/bll/RetraitManager.java
@@ -1,8 +1,20 @@
 package fr.eni.Encheres.bll;
 
+import fr.eni.Encheres.bo.Retrait;
+import fr.eni.Encheres.dal.DAOFactory;
+import fr.eni.Encheres.dal.RetraitDAO;
+
 public class RetraitManager {
 	
+	private RetraitDAO retraitDAO;
 	
+	public RetraitManager() {
+		retraitDAO = DAOFactory.getRetraitDAO();
+	}
+	
+	public void insertRetrait(Retrait retrait) {
+		retraitDAO.insert(retrait);
+	}
 	
 	
 }
--- Encheres/src/fr/eni/Encheres/dal/DAOFactory.java
+++ Encheres/src/fr/eni/Encheres/dal/DAOFactory.java
@@ -1,10 +1,13 @@
 package fr.eni.Encheres.dal;
 
+import fr.eni.Encheres.bll.RetraitManager;
+
 public class DAOFactory {
 	
 	private static UtilisateurDAO utilisateurDAO;
 	private static ArticleVenduDAO articleVenduDAO;
-
+	private static RetraitDAO retraitDAO;
+	
 	public static UtilisateurDAO getUtilisateurDAO() {
 		if (utilisateurDAO == null) {
 			utilisateurDAO = new UtilisateurDAOJdbcImpl();
@@ -17,6 +20,13 @@ public class DAOFactory {
 			articleVenduDAO = new ArticleVenduDAOJdbcImpl();
 		}
 		return articleVenduDAO;
+	}
+	
+	public static RetraitDAO getRetraitDAO() {
+		if(retraitDAO == null) {
+			retraitDAO = new RetraitDAOJdbcImpl();
+		}
+		return retraitDAO;
 	}
 	
 	
